aff738db4d1863d100ac5212eec591447409cdcd
Kristians Emils <emilstilugs@gmail.com>
2021-03-01 16:06:42 +0200

    1. lekcijas uzdevums pabeigts

---
+++ Assets/Scenes/Vards.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Vards : MonoBehaviour
+{
+    //Mainīgajā saglabās tekstu, ko raksta teksta laukā
+    public string teksts;
+    //Lauks kurā ieraksta tekstu
+    public GameObject ievadesLauks;
+    //Kur attēlot tekstu
+    public GameObject tekstaAttelosana;
+
+    //Funkcija tiek izsaukta nospiezot pogu
+    public void uzglabatTekstu()
+    {
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper() + " !";
+    }
+}
